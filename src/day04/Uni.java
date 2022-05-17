95fb05e8304f4f7460ebc4b346f8e87bb24dc532
ayKaraca <av.ahmetyasinkaraca@gmail.com>
2022-05-17 23:44:31 +0300

    day4

---
+++ src/day04/Uni.java
@@ -0,0 +1,54 @@
+package day04;
+
+public class Uni {
+    // str üniv, str bölüm, , int öğr sayısı, int notOrt olan
+    // pojo classtan 5 farklı liste oluştur
+    // ( plain old java object )
+
+    String uni;
+    String department;
+    int students;
+    int avGrade;
+
+    public String getUni() {
+        return uni;
+    }
+
+    public void setUni(String uni) {
+        this.uni = uni;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getStudents() {
+        return students;
+    }
+
+    public void setStudents(int students) {
+        this.students = students;
+    }
+
+    public int getAvGrade() {
+        return avGrade;
+    }
+
+    public void setAvGrade(int avGrade) {
+        this.avGrade = avGrade;
+    }
+
+    public Uni(String uni, String department, int students, int avGrade) {
+        this.uni = uni;
+        this.department = department;
+        this.students = students;
+        this.avGrade = avGrade;
+    }
+
+    public Uni() {
+    }
+}
